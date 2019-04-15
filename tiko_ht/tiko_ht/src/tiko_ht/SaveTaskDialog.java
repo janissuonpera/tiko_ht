783d8971f2df3a2cb52f2726feb295d8e334b3d0
Oskari Niskanen <oskari.niskanen97@gmail.com>
2019-04-15 20:58:21 +0300

    Update SaveTaskDialog.java
    Added functionality and bug fixes.

--- tiko_ht/tiko_ht/src/tiko_ht/SaveTaskDialog.java
+++ tiko_ht/tiko_ht/src/tiko_ht/SaveTaskDialog.java
@@ -40,7 +40,7 @@ public class SaveTaskDialog extends Dialog {
 	 */
 	public SaveTaskDialog(Shell parent, int style) {
 		super(parent, style);
-		setText("Tallenna ty�suoritus");
+		setText("Tallenna työsuoritus");
 	}
 	/**
 	 * Open the dialog.
@@ -69,92 +69,63 @@ public class SaveTaskDialog extends Dialog {
 		shell = new Shell(getParent(), SWT.DIALOG_TRIM);
 		shell.setSize(448, 272);
 		shell.setText(getText());
-		shell.setLayout(new GridLayout(5, false));
+		shell.setLayout(null);
+
+		Label queryResultLabel = new Label(shell, SWT.NONE);
+		queryResultLabel.setBounds(354, 189, 83, 15);
 
 		Label lblValitseKohde = new Label(shell, SWT.NONE);
-		lblValitseKohde.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblValitseKohde.setBounds(20, 9, 74, 15);
 		lblValitseKohde.setText("Valitse kohde*");
 
 		Combo job_dropdown = new Combo(shell, SWT.READ_ONLY);
+		job_dropdown.setBounds(115, 5, 234, 23);
 		db.connect();
 		jobs = db.getJobs();
+		job_dropdown.add("");
 		for (int i = 0; i < jobs.size(); i++) {
 			job_dropdown.add(jobs.get(i));
 		}
 
-		GridData gd_job_dropdown = new GridData(SWT.FILL, SWT.CENTER, true,
-				false, 3, 1);
-		gd_job_dropdown.widthHint = 195;
-		job_dropdown.setLayoutData(gd_job_dropdown);
-		new Label(shell, SWT.NONE);
-
 		Label lblAnnaPivmr = new Label(shell, SWT.NONE);
-		lblAnnaPivmr.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblAnnaPivmr.setBounds(9, 37, 97, 15);
 		lblAnnaPivmr.setText("Anna p\u00E4iv\u00E4m\u00E4\u00E4r\u00E4*");
 
 		DateTime date_time = new DateTime(shell, SWT.BORDER);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
+		date_time.setBounds(115, 33, 76, 24);
 
 		Label lblAnnaTehdytTunnit = new Label(shell, SWT.NONE);
-		lblAnnaTehdytTunnit.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblAnnaTehdytTunnit.setBounds(5, 65, 105, 15);
 		lblAnnaTehdytTunnit.setText("Anna tehdyt tunnit*");
 
 		Spinner hour_spinner = new Spinner(shell, SWT.BORDER);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
+		hour_spinner.setBounds(115, 62, 47, 22);
 
 		Label lblAnnaTynTyyppy = new Label(shell, SWT.NONE);
-		lblAnnaTynTyyppy.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblAnnaTynTyyppy.setBounds(9, 93, 96, 15);
 		lblAnnaTynTyyppy.setText("Anna ty\u00F6n tyyppi*");
 
 		Combo worktype_dropdown = new Combo(shell, SWT.READ_ONLY);
+		worktype_dropdown.setBounds(115, 89, 88, 23);
 		worktype_dropdown.setItems(
-				new String[]{"Ty\u00F6", "Suunnittelu", "Aputy\u00F6"});
-		GridData gd_worktype_dropdown = new GridData(SWT.FILL, SWT.CENTER, true,
-				false, 1, 1);
-		gd_worktype_dropdown.widthHint = 177;
-		worktype_dropdown.setLayoutData(gd_worktype_dropdown);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
+				new String[]{"", "Ty\u00F6", "Suunnittelu", "Aputy\u00F6"});
 
 		Label lblLisTarvike = new Label(shell, SWT.NONE);
-		lblLisTarvike.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 2));
+		lblLisTarvike.setBounds(22, 127, 71, 15);
 		lblLisTarvike.setText("Valitse tarvike");
 
 		Combo item_dropdown = new Combo(shell, SWT.READ_ONLY);
-
-		GridData gd_item_dropdown = new GridData(SWT.LEFT, SWT.CENTER, true,
-				false, 1, 2);
-		gd_item_dropdown.widthHint = 136;
-		item_dropdown.setLayoutData(gd_item_dropdown);
-		new Label(shell, SWT.NONE);
+		item_dropdown.setBounds(115, 123, 88, 23);
 		db.connect();
+		item_dropdown.add("");
 		itemList = db.getAllItems();
 		for (int i = 0; i < itemList.get(0).size(); i++) {
 			item_dropdown.add(itemList.get(0).get(i));
 		}
 
 		Button selectedItems_btn = new Button(shell, SWT.NONE);
-
-		GridData gd_selectedItems_btn = new GridData(SWT.CENTER, SWT.CENTER,
-				false, false, 1, 2);
-		gd_selectedItems_btn.heightHint = 26;
-		gd_selectedItems_btn.widthHint = 75;
-
-		selectedItems_btn.setLayoutData(gd_selectedItems_btn);
+		selectedItems_btn.setBounds(274, 121, 75, 26);
 		selectedItems_btn.setText("Tarvikelista");
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
 		selectedItems_btn.addListener(SWT.Selection, new Listener() {
 
 			@Override
@@ -169,25 +140,25 @@ public class SaveTaskDialog extends Dialog {
 		});
 
 		Label lblMr = new Label(shell, SWT.NONE);
-		lblMr.setLayoutData(
-				new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblMr.setBounds(41, 160, 33, 15);
 		lblMr.setText("M\u00E4\u00E4r\u00E4");
 
 		Spinner item_amount_spinner = new Spinner(shell, SWT.BORDER);
-		GridData gd_item_amount_spinner = new GridData(SWT.LEFT, SWT.CENTER,
-				false, false, 1, 1);
-		gd_item_amount_spinner.widthHint = 58;
-		item_amount_spinner.setLayoutData(gd_item_amount_spinner);
+		item_amount_spinner.setBounds(115, 157, 87, 22);
+		item_amount_spinner.setMaximum(9999);
+		item_amount_spinner.setMinimum(1);
 
 		Label lblAlennus = new Label(shell, SWT.NONE);
+		lblAlennus.setBounds(208, 160, 61, 15);
 		lblAlennus.setText("Alennus(%)");
 
 		Spinner discount_spinner = new Spinner(shell, SWT.BORDER);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
+		discount_spinner.setBounds(274, 157, 47, 22);
 
 		// Button for adding an item.
 		Button addItem_btn = new Button(shell, SWT.NONE);
+		addItem_btn.setBounds(115, 184, 76, 25);
+		addItem_btn.setEnabled(false);
 
 		// Add item button listener.
 		addItem_btn.addListener(SWT.Selection, new Listener() {
@@ -203,18 +174,13 @@ public class SaveTaskDialog extends Dialog {
 			}
 		});
 		addItem_btn.setText("Lis\u00E4\u00E4 tarvike");
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
 
 		Label lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setBounds(5, 219, 97, 15);
 		lblNewLabel.setText("* Tarvittavat tiedot");
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
 
 		Button cancel_btn = new Button(shell, SWT.NONE);
-		cancel_btn.setLayoutData(
-				new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		cancel_btn.setBounds(296, 214, 53, 25);
 		cancel_btn.setText("Peruuta");
 		cancel_btn.addListener(SWT.Selection, new Listener() {
 			@Override
@@ -225,20 +191,53 @@ public class SaveTaskDialog extends Dialog {
 
 		// Save data and add task.
 		Button done_btn = new Button(shell, SWT.NONE);
+		done_btn.setBounds(354, 214, 83, 25);
 		done_btn.setText("Lis\u00E4\u00E4 suoritus");
+		done_btn.setEnabled(false);
 		// Saves inserted information into db.
 		done_btn.addListener(SWT.Selection, new Listener() {
-
 			@Override
 			public void handleEvent(Event arg0) {
-				db.connect();
-				db.createTask(job_dropdown.getText(),
-						worktype_dropdown.getText(),
-						hour_spinner.getSelection(), date_time, selected_items,
-						item_amount, discount_pct);
+				if ((hour_spinner.getSelection() > 0
+						&& !worktype_dropdown.getText().equals(""))
+						|| item_amount_spinner.getSelection() > 0
+								&& !item_dropdown.getText().equals("")
+								&& !selected_items.isEmpty()) {
+					db.connect();
+					db.createTask(job_dropdown.getText(),
+							worktype_dropdown.getText(),
+							hour_spinner.getSelection(), date_time,
+							selected_items, item_amount, discount_pct);
+					job_dropdown.select(0);
+					item_dropdown.select(0);
+					worktype_dropdown.select(0);
+					queryResultLabel.setText("Lisäys onnistui!");
+				} else {
+					queryResultLabel.setText("Virhe annetuissa tiedoissa!");
+				}
 			}
 		});
-
+		// If job hasn't been selected, disable done button.
+		job_dropdown.addListener(SWT.Selection, new Listener() {
+			@Override
+			public void handleEvent(Event arg0) {
+				if (job_dropdown.getText().equals("")) {
+					done_btn.setEnabled(false);
+				} else {
+					done_btn.setEnabled(true);
+				}
+				queryResultLabel.setText("");
+			}
+		});
+		item_dropdown.addListener(SWT.Selection, new Listener() {
+			@Override
+			public void handleEvent(Event arg0) {
+				if (item_dropdown.getText().equals("")) {
+					addItem_btn.setEnabled(false);
+				} else {
+					addItem_btn.setEnabled(true);
+				}
+			}
+		});
 	}
-
 }
